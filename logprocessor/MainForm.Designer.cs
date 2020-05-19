41082c630d755816eb8cec0a73e3c359fd3f716e
Bernhard Sageder <bernhard@sageder.net>
2020-05-19 08:22:36 +0200

    Initial version

---
+++ logprocessor/MainForm.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace logprocessor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.analyzeButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView.Location = new System.Drawing.Point(0, 89);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(800, 361);
+            this.dataGridView.TabIndex = 5;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "\"csv files (*.csv)|*.csv|All files (*.*)|*.*\"";
+            this.openFileDialog.Multiselect = true;
+            this.openFileDialog.Title = "Select files to analyze ...";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.analyzeButton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 89);
+            this.panel1.TabIndex = 6;
+            // 
+            // analyzeButton
+            // 
+            this.analyzeButton.Location = new System.Drawing.Point(12, 12);
+            this.analyzeButton.Name = "analyzeButton";
+            this.analyzeButton.Size = new System.Drawing.Size(156, 29);
+            this.analyzeButton.TabIndex = 4;
+            this.analyzeButton.Text = "Choose files and analyze ...";
+            this.analyzeButton.UseVisualStyleBackColor = true;
+            this.analyzeButton.Click += new System.EventHandler(this.analyzeButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.panel1);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button analyzeButton;
+    }
+}
+
---
+++ logprocessor/MainForm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace logprocessor
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void analyzeButton_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                dataGridView.DataSource = LoadFileAndCreateTable(openFileDialog.FileName);
+            }
+        }
+
+        private DataTable LoadFileAndCreateTable(string fileName)
+        {
+            var csvLines = File.ReadAllLines(fileName, Encoding.Default);
+            var splittedLines = csvLines
+                .Select(x => x.Split(';'))
+                .ToList();
+            return CreateDataTable(splittedLines);
+        }
+
+        private DataTable CreateDataTable(List<string[]> splittedCsvLines)
+        {
+            DataTable table = new DataTable();
+            // Create columns
+            foreach (string columnName in splittedCsvLines[0])
+            {
+                table.Columns.Add(columnName, typeof(string));
+            }
+            // Create rows
+            foreach (var splittedCsvLine in splittedCsvLines.Skip(1))
+            {
+                table.Rows.Add(splittedCsvLine);
+            }
+            return table;
+        }
+    }
+}
